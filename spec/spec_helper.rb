bb0ff0c4d115a7871c69b7cfd196eb51950247d7
Matteo Collina <matteo.collina@gmail.com>
2007-06-25 11:14:49 +0000

    fixed spec loading.. again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
-require 'rubygems'
-require 'spec'
+unless Object.const_defined? :LLIP
+  $:.unshift(File.join(File.dirname(__FILE__), "/../lib/llip"))
 
-$:.unshift(File.join(File.dirname(__FILE__), "/../lib/llip"))
+  require File.join(File.dirname(__FILE__), "/../lib/llip")
 
-require 'llip'
+  include LLIP
 
-include LLIP
+  require 'rubygems'
+  require 'spec'
 
-$: << File.dirname(__FILE__) + "/../examples/ariteval"
+  $: << File.dirname(__FILE__) + "/../examples/ariteval"
+end
